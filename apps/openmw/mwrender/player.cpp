57d403aaa24d96a265280d977a76f372f90cc289
Chris Robinson <chris.kcat@gmail.com>
2012-10-20 18:47:56 -0700

    Allow the player scene node visibility changes to cascade

--- apps/openmw/mwrender/player.cpp
+++ apps/openmw/mwrender/player.cpp
@@ -132,8 +132,7 @@ namespace MWRender
             mAnimation->runAnimation(duration);
         }
         mPlayerNode->setVisible(
-            mVanity.enabled || mPreviewMode || !mFirstPersonView,
-            false
+            mVanity.enabled || mPreviewMode || !mFirstPersonView
         );
 
         if (mFirstPersonView && !mVanity.enabled) {
